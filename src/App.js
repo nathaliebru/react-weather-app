29adc28da806590b6243c891467669536bb6d67d
nathaliebru <92443285+nathaliebru@users.noreply.github.com>
2021-12-06 14:01:53 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
+      </div>
     </div>
   );
 }
