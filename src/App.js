4ecc86e530450eb8f582ca87854fe98e3821e2fc
nathaliebru <92443285+nathaliebru@users.noreply.github.com>
2021-12-06 14:08:24 +0100

    Added link to GitHub repo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/nathaliebru/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source coded
+          </a>{" "}
+          by Nathalie Bruinhof
+        </footer>
       </div>
     </div>
   );
